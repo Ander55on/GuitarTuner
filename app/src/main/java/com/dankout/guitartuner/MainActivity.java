ff966042e59c57c55a2f10af13b5d18cee69c530
Ander55on <Anderzzon_91@hotmail.com>
2021-03-30 21:17:49 +0200

    added some simple UI

--- app/src/main/java/com/dankout/guitartuner/MainActivity.java
+++ app/src/main/java/com/dankout/guitartuner/MainActivity.java
@@ -1,14 +1,19 @@
 package com.dankout.guitartuner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
 
     //Configurations for the Audio recorder
     private static final int RECORDING_SAMPLE_RATE = 44100;
@@ -25,5 +30,22 @@ public class MainActivity extends AppCompatActivity {
 
         //Returns the minimum buffer size for the successful creation of an AudioRecord object
         this.mBufferSize = AudioRecord.getMinBufferSize(RECORDING_SAMPLE_RATE, AUDIO_CHANNEL_CONFIGURATION, AUDIO_FORMAT_ENCODING);
+
+        mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC,RECORDING_SAMPLE_RATE,
+                AUDIO_CHANNEL_CONFIGURATION, AUDIO_FORMAT_ENCODING, mBufferSize);
+    }
+
+    public void record(View view) {
+
+       Drawable background = null;
+
+       if(mRecorder.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) {
+
+       } else {
+           view.setActivated(true);
+       }
+
+
+
     }
 }
